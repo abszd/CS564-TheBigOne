6d3f10c9607d4b26c909cdaa80b3ebfe0a9b29c1
bobo mojo <ghadcock@wisc.edu>
2025-03-19 14:15:19 -0500

    Header comments + id

--- part3/buf.C
+++ part3/buf.C
@@ -1,6 +1,6 @@
 // Arian Abbaszadeh - 9083678194
-// Glenn Hadcock -
-// Akshaya Somasundaram -
+// Glenn Hadcock - 9085633288
+// Akshaya Somasundaram - 9085417682
 // Sets up the buffer manager class and functions to interact with the buffer manager
 
 #include <memory.h>
@@ -49,6 +49,9 @@ BufMgr::BufMgr(const int bufs)
     clockHand = bufs - 1;
 }
 
+/**
+ * Buffer manager destructor. Flushes out unwritten pages and deallocates/deletes the buffer pool and buffer table.
+ */
 BufMgr::~BufMgr()
 {
 
@@ -72,12 +75,19 @@ BufMgr::~BufMgr()
     delete[] bufPool;
 }
 
+/**
+ * Allocates a buffer frame for a page. This is like creating the actual buffer frame for a page for our examples in class.
+ * @param {int} frame - The frame number to allocate
+ * @returns {Status} OK if successful, BUFFEREXCEEDED if the buffer is full, UNIXERR if the call to the I/O layer returned an error when a dirty page was being written to disk, and HASHTBLERROR if the page was not found in the hash table (or if it just fails to remove it)
+ */
+
 const Status BufMgr::allocBuf(int &frame)
 {
     int checked = 0;
     // if we've checked all frames twice to allow for the reference bit then we're at capacity and checked is being incremented here
     while (checked++ < numBufs * 2)
     {
+        // Advance the clock hand. Does this first because clockHand is initialized to numBufs - 1. Advancing it will cause it to wrap around to 0
         advanceClock();
         BufDesc *curBuf = &bufTable[clockHand]; // current frame we're checking
 
@@ -122,6 +132,13 @@ const Status BufMgr::allocBuf(int &frame)
     return BUFFEREXCEEDED; // If we reach this weve tried every frame and allowed for the reference bit to switch
 }
 
+/**
+ * Makes sure a page is loaded into the buffer pool. If not, a frame will be allocated for it and it will be read from disk. This is like the request method from class slides.
+ * @param {File*} file - The file to read the page from
+ * @param {int} PageNo - The page number to read
+ * @param {Page*} page - The pointer to the page to read
+ * @returns {Status} OK if successful and everything works as expected, HASHNOTFOUND if the page was not found in the hash table, UNIXERR if the call to the I/O layer returned an error when a dirty page was being written to disk or errored when reading the page from disk, and HASHTBLERROR if the page was failed to be inserted into the buffer pool
+ */
 const Status BufMgr::readPage(File *file, const int PageNo, Page *&page)
 {
     int framePtr = 0;
@@ -154,9 +171,17 @@ const Status BufMgr::readPage(File *file, const int PageNo, Page *&page)
     return OK;
 }
 
+/**
+ * Equivalent to releasing a page from the buffer pool. If dirty was true, the dirty bit will be set to true. Otherwise, it will remain the same. Returns OK if there are no errors. This is like the release method from class slides and the modify and release method in one if the dirty bit is true.
+ * @param {File*} file - The file to unpin the page from
+ * @param {int} PageNo - The page number to unpin from the buffer pool
+ * @param {bool} dirty - Whether the page is dirty or not
+ * @returns {Status} OK if successful and everything works as expected, HASHNOTFOUND if the page was not found in the buffer pool hash table, and PAGENOTPINNED if the pin count was already 0
+ */
 const Status BufMgr::unPinPage(File *file, const int PageNo,
                                const bool dirty)
 {
+    // First we need to make sure the page is in the buffer pool
     int framePtr = 0;
     Status pageStatus = hashTable->lookup(file, PageNo, framePtr);
 
@@ -168,7 +193,7 @@ const Status BufMgr::unPinPage(File *file, const int PageNo,
     BufDesc &curFrame = bufTable[framePtr];
     if (curFrame.pinCnt > 0)
     {
-        // updat the pincount and dirty bit
+        // update the pincount and dirty bit
         curFrame.pinCnt--;
         curFrame.dirty = curFrame.dirty | dirty;
         return OK;
@@ -176,6 +201,13 @@ const Status BufMgr::unPinPage(File *file, const int PageNo,
     return PAGENOTPINNED; // We don't want to increment the count if its not pinned, this could cause allocation issues
 }
 
+/**
+ * Allocates a new page in the file and makes sure it is in the buffer pool. This is like the allocate method from class slides.
+ * @param {File*} file - The file to allocate the page in
+ * @param {int} pageNo - The page number to allocate
+ * @param {Page*} page - The pointer to the page to allocate
+ * @returns {Status} OK if successful and everything works as expected, UNIXERR if a UNIX error occurs, BUFFEREXCEEDED if all the buffer frames are pinned (in allocBuf), HASHTBLERROR if a hash table error occurs.
+ */
 const Status BufMgr::allocPage(File *file, int &pageNo, Page *&page)
 {
     if (file->allocatePage(pageNo) != OK) // Make the page
@@ -199,6 +231,12 @@ const Status BufMgr::allocPage(File *file, int &pageNo, Page *&page)
     return OK;
 }
 
+/**
+ * Disposes of a page in the file. Releases the page pageNo and adds it to the free list.
+ * @param {File*} file - The file to dispose of the page in
+ * @param {int} pageNo - The page number to dispose of
+ * @returns {Status} OK if successful and everything works as expected, BADPAGENO if pageNo is not a valid page and UNIXERR if a UNIX error occurs.
+ */
 const Status BufMgr::disposePage(File *file, const int pageNo)
 {
     // see if it is in the buffer pool
@@ -216,6 +254,11 @@ const Status BufMgr::disposePage(File *file, const int pageNo)
     return file->disposePage(pageNo);
 }
 
+/**
+ * Flushes all pages in the buffer pool that are associated with the file. If the page is dirty, it will be flushed to disk and the dirty bit will be set to false. It will also remove the page from the hash table (regardless of whether it is dirty or not) and will call the clear method on next frame.
+ * @param {File*} file - The file to flush the pages of
+ * @returns {Status} OK if successful and everything works as expected, PAGEPINNED if some page is pinned still.
+ */
 const Status BufMgr::flushFile(const File *file)
 {
     Status status;
@@ -256,6 +299,9 @@ const Status BufMgr::flushFile(const File *file)
     return OK;
 }
 
+/**
+ * Prints the status of the buffer pool. Just like the table that was shown in class (with the pin count, valid bit, etc.)
+ */
 void BufMgr::printSelf(void)
 {
     BufDesc *tmpbuf;
