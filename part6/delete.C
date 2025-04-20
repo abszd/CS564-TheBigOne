eb6c6dc2ece6d9e52dc0933d2202fa01a130680a
Arian Abbaszadeh <abbaszadeharian@gmail.com>
2025-04-19 21:01:55 -0500

    made delete

--- part6/delete.C
+++ part6/delete.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
 
-
 /*
  * Deletes records from a specified relation.
  *
@@ -10,17 +9,53 @@
  * 	an error code otherwise
  */
 
-const Status QU_Delete(const string & relation, 
-		       const string & attrName, 
-		       const Operator op,
-		       const Datatype type, 
-		       const char *attrValue)
+const Status QU_Delete(const string &relation,
+					   const string &attrName,
+					   const Operator op,
+					   const Datatype type,
+					   const char *attrValue)
 {
-// part 6
-return OK;
+	// part 6
+	Status status;
+	AttrDesc attrrec;
 
+	HeapFileScan *hfs = new HeapFileScan(relation, status);
+	status = attrCat->getInfo(relation, attrName, attrrec);
+	if (status)
+	{
+		return status;
+	}
 
+	float ffltr;
+	int ifltr;
+	char *fltr;
+	fltr = (char *)attrValue;
+	if (type == INTEGER)
+	{
+		ifltr = atoi(attrValue);
+		fltr = (char *)&ifltr;
+	}
+	else if (type == FLOAT)
+	{
+		ffltr = atof(attrValue);
+		fltr = (char *)&ffltr;
+	}
+	hfs->startScan(attrrec.attrOffset, attrrec.attrLen, type, fltr, op);
 
+	Record rec;
+	RID rid;
+	while (!(status = hfs->scanNext(rid)))
+	{
+		status = hfs->deleteRecord();
+		if (status)
+		{
+			return status;
+		}
+	}
+	if (status == FILEEOF)
+	{
+		status = OK;
+	}
+	hfs->endScan();
+	return status;
 }
-
-
