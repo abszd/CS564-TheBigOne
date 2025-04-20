44086425ebe3161f13f483e17e90870772b9d914
glennh2023 <glenn.hadcock@gmail.com>
2025-04-20 15:37:11 -0500

    B

--- part6/insert.C
+++ part6/insert.C
@@ -1,3 +1,4 @@
+// NOT THIS ONE
 #include "catalog.h"
 #include "query.h"
 
@@ -9,62 +10,106 @@
  * 	an error code otherwise
  */
 
-const Status QU_Insert(const string &relation,
-					   const int attrCnt,
-					   const attrInfo attrList[])
+const Status QU_Insert(const string & relation, 
+                      const int attrCnt, 
+                      const attrInfo attrList[])
 {
-	// part 6
-	Status status;
-	int relcnt;
-	AttrDesc *rels = new AttrDesc[attrCnt];
-	status = attrCat->getRelInfo(relation, relcnt, rels);
-	if (status)
-	{
-		return status;
-	}
-	if (relcnt < attrCnt)
-	{
-		return ATTRTOOLONG;
-	}
-	int reccnt = 0;
-	for (int i = 0; i < relcnt; i++)
-	{
-		reccnt += rels[i].attrLen;
-	}
-	char *buf = new char[reccnt];
-	for (int i = 0; i < relcnt; i++)
-	{
-		bool found = false;
-		for (int j = 0; j < attrCnt; j++)
-		{
-			if (attrList[j].attrValue == NULL)
-			{
-				return ATTRNOTFOUND;
-			}
-			if (!strcmp(rels[i].attrName, attrList[j].attrName)) // if names are the same
-			{
-				memcpy(buf + rels[i].attrOffset,
-					   attrList[j].attrValue,
-					   rels[i].attrLen);
-				found = true;
-				break;
-			}
-		}
-		if (!found)
-		{
-			return ATTRNOTFOUND;
-		}
-	}
-	InsertFileScan *ifs = new InsertFileScan(relation, status);
-	if (status)
-	{
-		return status;
-	}
+    Status status;
+    int cnt;
+    AttrDesc *attrDescArray = nullptr;
+    char *outputData = nullptr;
+    InsertFileScan *resultRel = nullptr;
 
-	Record rec;
-	rec.data = buf;
-	rec.length = reccnt;
+    try {
+        // Get relation info
+        if ((status = attrCat->getRelInfo(relation, cnt, attrDescArray)) != OK) {
+            return status;
+        }
 
-	RID rid;
-	return ifs->insertRecord(rec, rid);
+        // Calculate record length
+        int reclen = 0;
+        for (int i = 0; i < cnt; i++) {
+            reclen += attrDescArray[i].attrLen;
+        }
+
+        // Allocate output buffer
+        outputData = new char[reclen];
+        
+        // Build record
+        int outputOffset = 0;
+        for (int i = 0; i < cnt; ++i) {
+            bool found = false;
+            for (int j = 0; j < attrCnt; ++j) {
+                if (!attrList[j].attrValue) {
+                    delete[] outputData;
+                    delete[] attrDescArray;
+                    return ATTRNOTFOUND;
+                }
+                
+                if (strcmp(attrDescArray[i].attrName, attrList[j].attrName) == 0) {
+                    // Handle different types
+                    char tempBuffer[attrDescArray[i].attrLen];
+                    void *ptr = tempBuffer;
+                    
+                    switch(attrList[j].attrType) {
+                        case STRING:
+                            ptr = attrList[j].attrValue;
+                            break;
+                        case INTEGER: {
+                            int value = atoi((char*)attrList[j].attrValue);
+                            memcpy(tempBuffer, &value, sizeof(int));
+                            break;
+                        }
+                        case FLOAT: {
+                            float value = atof((char*)attrList[j].attrValue);
+                            memcpy(tempBuffer, &value, sizeof(float));
+                            break;
+                        }
+                        default:
+                            delete[] outputData;
+                            delete[] attrDescArray;
+                            return ATTRTYPEMISMATCH;
+                    }
+                    
+                    memcpy(outputData + outputOffset, ptr, attrDescArray[i].attrLen);
+                    outputOffset += attrDescArray[i].attrLen;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                delete[] outputData;
+                delete[] attrDescArray;
+                return ATTRNOTFOUND;
+            }
+        }
+
+        // Insert the record
+        Record outputRec;
+        outputRec.data = outputData;
+        outputRec.length = reclen;
+
+        resultRel = new InsertFileScan(relation, status);
+        if (status != OK) {
+            delete[] outputData;
+            delete[] attrDescArray;
+            return status;
+        }
+
+        RID outRID;
+        status = resultRel->insertRecord(outputRec, outRID);
+
+        // Cleanup
+        delete[] outputData;
+        delete[] attrDescArray;
+        delete resultRel;
+        
+        return status;
+
+    } catch (...) {
+        delete[] outputData;
+        delete[] attrDescArray;
+        delete resultRel;
+        throw;
+    }
 }
